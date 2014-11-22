757c5d555e9d6dc553fb0a21659661258e0f89cc
Mark Walkowski <markwalkowski@mac.com>
2014-11-22 11:09:24 -0600

    Completed the if/else assignment

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,15 +12,25 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
+    if (dollars < 5) {
+        itemToReturn = @"get out of my store";
+    } else if (dollars < 6){
+        itemToReturn = @"have some gum";
+    } else if (dollars < 1000){
+        itemToReturn = @"have an apple";
+    } else if (dollars < 1000000000){
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars >= 1000000000){
+        itemToReturn = @"have The Big Apple";
+    }
 
-    /* WORK HERE */
+
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
 
     NSUInteger cost = 24;
     
