f9bf38bf79f5d4c6d6181b3d9e0613ec8281860c
Mark Walkowski <markwalkowski@mac.com>
2014-11-23 10:37:56 -0600

    Completed Loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,7 +11,28 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    return @"";
+    NSMutableString *modifiableStatement = [@"" mutableCopy];
+    NSInteger number1 = 0;
+    NSInteger number2 = 0;
+
+    BOOL isNumberGreatThanOtherNumber = (number >= otherNumber);
+    if (isNumberGreatThanOtherNumber) {
+        number2 = number;
+        number1 = otherNumber;
+
+    }else{
+        number1 = number;
+        number2 = otherNumber;
+    }
+    
+    
+    NSString *string = @"";
+    for (;number1 < number2+1;number1++) {
+        string = [NSString stringWithFormat:@"%ld", (long)number1];
+        [modifiableStatement appendString:string];
+    }
+    
+    return modifiableStatement;
 }
 
 @end
